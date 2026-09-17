fd64fab3bfda129a95b0bb937c5a8f0f08da065b
peterpaul10m-code <peterpaul10m@gmail.com>
2026-09-17 14:40:51 -0500

    Use compressed v7 server bundle

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const fs=require('fs');
 const zlib=require('zlib');
-const parts=[1,2,3,4].map(i=>fs.readFileSync(`server.part${i}.txt`,'utf8').trim()).join('');
-const source=zlib.gunzipSync(Buffer.from(parts,'base64')).toString('utf8');
+const bundle=fs.readFileSync('server.bundle.txt','utf8').trim();
+const source=zlib.gunzipSync(Buffer.from(bundle,'base64')).toString('utf8');
 eval(source);
