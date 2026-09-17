455c29c2a4e321c7e72ba7825544b4cdeac4d8b1
peterpaul10m-code <peterpaul10m@gmail.com>
2026-09-17 14:40:40 -0500

    Restore server through compressed v7 source loader

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-PLACEHOLDER
+const fs=require('fs');
+const zlib=require('zlib');
+const parts=[1,2,3,4].map(i=>fs.readFileSync(`server.part${i}.txt`,'utf8').trim()).join('');
+const source=zlib.gunzipSync(Buffer.from(parts,'base64')).toString('utf8');
+eval(source);
